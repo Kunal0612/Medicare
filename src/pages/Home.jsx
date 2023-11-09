cd6606d4e0205493fece155b0c502de2e8811cfe
Kunal Shaw <kunalshaw865@gmail.com>
2023-11-10 01:16:28 +0530

    Day 3 added feature about and services section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,11 @@ import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
 import {BsArrowRight} from 'react-icons/bs'
 import {Link} from 'react-router-dom'
+import About from '../components/about/About'
+import ServiceList from '../components/services/ServiceList'
+import featureImg from '../assets/images/feature-img.png'
+import videoicon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png'
 const Home = () => {
   return <>
   {/* Hero Section */}
@@ -138,6 +143,78 @@ const Home = () => {
         </div>
       </div>
   </section>
+  {/* About Section */}
+  <About/>
+  {/* services section */}
+  <section>
+    <div className="container">
+      <div className='xl:w-[470px] mx-auto'>
+        <h2 className='heading text-center'>Our medical Services</h2>
+        <p className='text_para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim eos magnam pariatur fugit voluptate.</p>
+      </div>
+      <ServiceList/>
+    </div>
+  </section>
+  {/* Feature Section */}
+  <section>
+    <div className='container'>
+      <div className='flex items-center justify-between flex-col lg:flex-row'>
+        {/* feature content */}
+        <div className='xl:w-[670px]'>
+          <h2 className='heading'>Get virtual Treatment <br/> anytime</h2>
+          <ul className="pl-4">
+            <li className='text_para'>
+              1. Schedule the Appointment directly
+            </li>
+            <li className="text_para">
+              2.Schedule for physician here,and contact their office
+            </li>
+            <li className="text_para">
+              3.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus itaque tenetur in!
+            </li>
+          </ul>
+          <Link to='/'>
+              <button className='btn'>Learn More</button>
+          </Link>
+        </div>
+        {/* Feature */}
+        <div className='relative z-10 xl:w-[770] flex justify-end mt-[50px]
+          lg:mt-0'>
+            <img src={featureImg} className='w-3/4' alt="" />
+            <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px]
+            left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:pb-[26px]
+            rounded-[10px]'>
+              <div className='flex items-center justify-between'>
+                <div className='flex items-center gap-[6px] lg:gap-3'>
+                  <p className='text-[10px] leading-[10px] lg:text-[14px] 
+                  text-headingColor font-[600]'>Fri,30</p>
+                  <p className='text-[10px] leading-[10px] lg:text-[14px] 
+                  text-textColor font-[500]'>11:00</p>
+                </div>
+                <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center
+                justify-center bg-yellowColor rounded py-1 px-[6px]
+                lg:py-3 lg-px-[9px]'>
+                  <img src={videoicon} alt="" />
+                </span>
+              </div>
+              <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1
+              px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px]
+              lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500]
+              mt-2 lg:mt-4 rounded-full'>
+                Consultation
+              </div>
+              <div className='flex items-cneter gap-[6px] lg:gap-[10px]
+              '>
+                <img src={avatarIcon} alt="" />
+                <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700]
+                text-headingColor'>Glenn Maxwell</h4>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+  
   </>
   </>
 }
